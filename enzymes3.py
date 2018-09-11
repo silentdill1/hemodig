36f5a1f61644ab56078f968ed48b2354a5aa0e33
friedrich <frputtkammer@gmail.com>
2018-09-11 17:10:30 +0200

    randomized motif selection in configuration of cleavage sites

--- enzymes3.py
+++ enzymes3.py
@@ -1,4 +1,5 @@
 import peptides
+import numpy as np
 
 
 class Enzyme:
@@ -15,50 +16,61 @@ class Enzyme:
         self.maxEnzymeAbundance = max_enzyme_abundance  # in fmol
 
     def configure_cleavage_sites(self, peptide):
-        available_amino_acids = []
+        available_amino_acid_indices = []
+        # indices of amino acids in peptide who are not yet part of a cleavage site
+        available_motif_indices = []
         # indices of amino acids in peptide who are not yet part of a cleavage site
         for i in range(0, peptide.length):
-            available_amino_acids.append(i)
-        for motif in self.cleavageMotifs:  # motif = tuple of AA Code and LiN, e.g. ('X', 4)
-            index_of_index_of_amino_acid = 0
-            while index_of_index_of_amino_acid < len(available_amino_acids):
-                index_of_amino_acid = available_amino_acids[index_of_index_of_amino_acid]
-                amino_acid = peptide.sequence[index_of_amino_acid]
-                if index_of_amino_acid != (peptide.length-1) and amino_acid == motif[0]:  # no cleavage after last AA
+            available_amino_acid_indices.append(i)
+        for i in range(0, len(self.cleavageMotifs)):
+                available_motif_indices.append(i)
+        while len(available_motif_indices) != 0:
+            index_of_motif_index = np.random.randint(0, len(available_motif_indices))  # pick first AS for cleavage site randomly
+            # TODO: randomized changes between current motif (currently looks for 'A' throughout the whole peptide chain,
+            # TODO: until it takes new motif for first AA of cleavage site)
+            motif_index = available_motif_indices[index_of_motif_index]
+            motif = self.cleavageMotifs[motif_index]  # motif = tuple of AA Code and LiN, e.g. ('X', 4)
+            index_of_amino_acid_index = 0
+
+            while index_of_amino_acid_index < len(available_amino_acid_indices):
+                amino_acid_index = available_amino_acid_indices[index_of_amino_acid_index]
+                amino_acid = peptide.sequence[amino_acid_index]
+                if amino_acid_index != (peptide.length-1) and amino_acid == motif[0]:  # no cleavage after last AA
                     cleavage_site_index = len(peptide.cleavageSites)
                     # save index in cleavage site array for future addition of LiNs for AAs in vicinity
-                    peptide.cleavageSites.append([index_of_amino_acid, motif[1]])  # create new cleavage site
+                    peptide.cleavageSites.append([amino_acid_index, motif[1]])  # create new cleavage site
                     # motif[1] is the LiN for current cleavage motif
-                    print(amino_acid+' 1st')
-                    available_amino_acids.remove(index_of_amino_acid)
+                    available_amino_acid_indices.remove(amino_acid_index)
                     # current amino acid is no longer available as cleavage option,
                     # since a cleavage site there is already taken into account
                     for i in range(-3, 4):  # check nearby amino acids for changes in cleavage likelihood
                         if i == 0:
                             continue  # no need to check set for removed amino acid
                         else:
-                            index_of_nearby_amino_acid = index_of_amino_acid + i
+                            index_of_nearby_amino_acid = amino_acid_index + i
 
-                        if index_of_nearby_amino_acid in available_amino_acids:
+                        if index_of_nearby_amino_acid in available_amino_acid_indices:
                             for motif2 in self.cleavageMotifs:  # check all motifs again, therefore different variable name
                                 if peptide.sequence[index_of_nearby_amino_acid] == motif2[0]:
                                     peptide.cleavageSites[cleavage_site_index][1] += motif2[1]  # add LiN of motif
                                     # cleavageSites = list of lists of position after which cut happens and LiN, e.g. [112, 5]
-                                    print(peptide.sequence[index_of_nearby_amino_acid]+' 2nd')
-                                    available_amino_acids.remove(index_of_nearby_amino_acid)  # amino acid no longer available for cleavage site
+                                    available_amino_acid_indices.remove(index_of_nearby_amino_acid)  # amino acid no longer available for cleavage site
                                     if i < 0:  # removing AAs index before current index -> moves and would skip one index
-                                        index_of_index_of_amino_acid -= 1
+                                        index_of_amino_acid_index -= 1
                 else:
-                    index_of_index_of_amino_acid += 1
+                    index_of_amino_acid_index += 1
+
+            available_motif_indices.remove(motif_index)
 
 
 hydrophobicAminoAcidMotifs = (('A', 1), ('G', 1), ('H', 1), ('I', 1), ('L', 1), ('M', 1), ('F', 1),
                               ('P', 1), ('V', 1))
 plas2 = Enzyme('Plasmepsin II', 'PF14_0077', hydrophobicAminoAcidMotifs, 500 * 10**3, 30 * 10**(-6))
-testSequence = ('X', 'X', 'X', 'A', 'L', 'X', 'X', 'X', 'X', 'A', 'T', 'L', 'F')
+testSequence = ('A', 'X', 'X', 'A', 'L', 'X', 'X', 'X', 'X', 'A', 'T', 'L', 'F', 'L', 'L', 'X', 'X', 'A', 'T', 'L', 'F')
 pep = peptides.Peptide(testSequence, len(testSequence))
 plas2.configure_cleavage_sites(pep)
 print(pep.cleavageSites)
+
 
 
 
