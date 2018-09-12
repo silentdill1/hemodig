2c91558f019dabaa1de7b48490d171f544b6d2fc
friedrich <frputtkammer@gmail.com>
2018-09-12 15:20:45 +0200

    added Motifs, fixed exopeptide implementation, began add function for motif tuples

--- enzymes3.py
+++ enzymes3.py
@@ -3,12 +3,12 @@ import numpy as np
 import sys
 
 
-class Nuclease:
-    def __init__(self, name, pf_id_old, is_exonuclease, cleavage_motifs, k_cat_k_m, max_enzyme_abundance,
-                 min_length, max_length, start_aa_index=-1, end_aa_index=-1):
+class Peptidase:
+    def __init__(self, name, pf_id_old, is_exopeptidase, cleavage_motifs, k_cat_k_m, max_enzyme_abundance,
+                 min_length, max_length, start_aa_index=-1, end_aa_index=-1, amino_peptidase_index=-1):
         self.name = name
         self.pfidOld = pf_id_old
-        self.isExonuclease = is_exonuclease
+        self.isExopeptidase = is_exopeptidase
         self.cleavageMotifs = cleavage_motifs
         # tuple of tuples containing amino acid in one letter code and corresponding change
         # in likelihood number (LiN), e.g. ('R', 3) means if 'R' is found in the vicinity of
@@ -19,10 +19,11 @@ class Nuclease:
         self.maxEnzymeAbundance = max_enzyme_abundance  # in fmol
         self.minLength = min_length  # minimum length of peptide that could be cleaved
         self.maxLength = max_length
-        self.startAAIndex = start_aa_index  # index of first amino acid that could be cleavage center for exonuclease
-        self.endAAIndex = end_aa_index  # index of last amino acid that could be cleavage center for exonuclease
+        self.startAAIndex = start_aa_index  # index of first amino acid that could be cleavage center for exopeptidase
+        self.endAAIndex = end_aa_index  # index of last amino acid that could be cleavage center for exopeptidase
+        self.aminoPeptidaseIndex = amino_peptidase_index
 
-    def configure_cleavage_sites(self, peptide):
+    def configure_cleavage_sites(self, peptide):  # for endopeptidases, falcilysin and dpap
         peptide.cleavageSites = []  # resetting attributes
         peptide.sumOfLiNsForAllCleavageSites = 0
 
@@ -31,9 +32,9 @@ class Nuclease:
         available_motif_indices = []
         # indices of amino acids in peptide who are not yet part of a cleavage site
 
-        if self.isExonuclease:  # Exonuclease operates only on N terminus of protein
+        if self.isExopeptidase:  # falcilysin operates only on N terminus of protein
             if self.startAAIndex == -1:
-                sys.exit('No startAAIndex for exonuclease')
+                sys.exit('No startAAIndex for exopeptidase')
             for i in range(self.startAAIndex, self.endAAIndex+1):
                 available_amino_acid_indices.append(i)
         else:
@@ -41,7 +42,8 @@ class Nuclease:
                 available_amino_acid_indices.append(i)
 
         found_cleavage_site = False
-        # is used only if enzyme is exonuclease,
+        # is used only for falcilysin and dpap exonucleases,
+        # because their cleavage likelihood depends on amino acids in vicinity,
         # changed to true after first cleavage site is found and terminates search
 
         for i in range(0, len(self.cleavageMotifs)):
@@ -83,26 +85,76 @@ class Nuclease:
                                     if i < 0:  # removing AAs index before current index -> moves and would skip one index
                                         index_of_amino_acid_index -= 1
                     peptide.sumOfLiNsForAllCleavageSites += LiN
-                    if self.isExonuclease:
+                    if self.isExopeptidase:
                         found_cleavage_site = True
                 else:
                     index_of_amino_acid_index += 1
 
             available_motif_indices.remove(motif_index)
 
+    def first_aa_cleavage(self, peptide):  # for single aa cleavage
+        peptide.cleavageSites = []  # resetting attributes
+        peptide.sumOfLiNsForAllCleavageSites = 0
+
+        if self.aminoPeptidaseIndex == -1:
+            sys.exit('No AminopeptidaseIndex given')
+        else:
+            api = self.aminoPeptidaseIndex  # index of amino acid to be checked for motif compatibility
+
+        for motif in self.cleavageMotifs:
+            if peptide.sequence[api] == motif[0]:
+                peptide.cleavageSites.append([0, motif[1]])
+                peptide.sumOfLiNsForAllCleavageSites = 1
+
+
+def add_motif_tuples(motif_tuple1, motif_tuple2):
+    list_sum_of_tuples = []
+    doubles = []
+    for motif_index1 in range(0, len(motif_tuple1)):
+        for motif_index2 in range(0, len(motif_tuple2)):
+            motif1 = motif_tuple1[motif_index1]
+            motif2 = motif_tuple2[motif_index2]
+            if motif1[0] == motif2[0]:
+                doubles.append([motif_index1, motif_index2])
+    for i in range(0, len(motif_tuple1)):
+        no_double = True
+        for pair in doubles:
+            if i == pair[0]:
+                no_double = False
+        if no_double:
+            list_sum_of_tuples.append(motif_tuple1[i])
+
+    for i in range(0, len(motif_tuple2)):
+            no_double = True
+            for pair in doubles:
+                if i == pair[1]:
+                    no_double = False
+                if no_double:
+                    list_sum_of_tuples.append(motif_tuple2[i])
+
+
+
+
 
 hydrophobicAminoAcidMotifs = (('A', 1), ('G', 1), ('H', 1), ('I', 1), ('L', 1), ('M', 1), ('F', 1),
                               ('P', 1), ('V', 1))
-plas2 = Nuclease('Plasmepsin II', 'PF14_0077', False, hydrophobicAminoAcidMotifs, 500 * 10**3, 30 * 10**(-6), 146, 80)
-fln = Nuclease('Falcilysin', '', True, hydrophobicAminoAcidMotifs, 148 * 10**3, 80 * 10**(-6), 25, 8, 0, 8)
-testSequence = ('A', 'X', 'X', 'A', 'L', 'X', 'X', 'X', 'X', 'A', 'T', 'L', 'F', 'L', 'L', 'X', 'X', 'A', 'T', 'L', 'F')
+chargedAminoAcidMotifs = (('D', -10), ('E', -10), ('K', -10))
+iPepMotifs = (('F', 2), ('I', 2))
+lPepMotifs = (('R', 2), ('L', 2), ('F', 2))
+flnMotifs = (('E', 2), ('M', 2), ('H', 2), ('S', 3), ('F', 2))
+proApMotif = [('P', 1)]
+
+
+plas2 = Peptidase('Plasmepsin II', 'PF14_0077', False, hydrophobicAminoAcidMotifs, 500 * 10**3, 30 * 10**(-6), 146, 80)
+fln = Peptidase('Falcilysin', '', True, hydrophobicAminoAcidMotifs, 148 * 10**3, 80 * 10**(-6), 25, 8, 0, 8)
+proAp = Peptidase('Prolin Aminopeptidase', '', True, tuple(proApMotif), 148 * 10**3, 80 * 10**(-6), 4, 2, 0, 0)
+testSequence = ('P', 'X', 'X', 'A', 'L', 'X', 'X', 'X', 'X', 'A', 'T', 'L', 'F', 'L', 'L', 'X', 'X', 'A', 'T', 'L', 'F')
 pep = peptides.Peptide(testSequence, len(testSequence))
 plas2.configure_cleavage_sites(pep)
 fln.configure_cleavage_sites(pep)
+proAp.
 print(pep.cleavageSites)
 print(pep.sumOfLiNsForAllCleavageSites)
-
-
 
 
 
